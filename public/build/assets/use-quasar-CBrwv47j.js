af7b638b43c1c779748427fac5dc72a97d82cf3f
Raunak <rlpuia@outlook.com>
2025-11-09 18:59:13 +0530

    New build

---
+++ public/build/assets/use-quasar-CBrwv47j.js
@@ -0,0 +1,1 @@
+import{aw as a,ax as r}from"./app-TSjbvmT9.js";function u(){return a(r)}export{u};
--- public/build/assets/use-quasar-CGEsHPfv.js
+++
@@ -1,1 +0,0 @@
-import{aw as a,ax as r}from"./app-Cdfm2waC.js";function u(){return a(r)}export{u};
